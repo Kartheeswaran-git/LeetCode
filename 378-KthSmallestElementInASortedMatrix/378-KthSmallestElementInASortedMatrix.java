bc170673bc273df5d4964ceadbc98d30e3657f08
Karthees <kartheeswaran2201@gmail.com>
2026-03-26 16:19:54 +0530

    Create 378-KthSmallestElementInASortedMatrix

---
+++ 378-KthSmallestElementInASortedMatrix/378-KthSmallestElementInASortedMatrix.java
@@ -0,0 +1,23 @@
+// Last updated: 26/03/2026, 16:19:53
+class Solution {
+    public int kthSmallest(int[][] matrix, int k) {
+       PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        
+        int n = matrix.length;
+        
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<n;j++){
+                if(pq.size() < k){
+                    pq.add(matrix[i][j]);
+                }else{
+                    if(matrix[i][j] < pq.peek()){
+                        pq.poll();
+                        pq.add(matrix[i][j]);
+                    }
+                }
+            }
+        }
+        
+        return pq.peek();
+    }
+}
