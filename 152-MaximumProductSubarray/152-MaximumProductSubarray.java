49d53c2925c008c2b75aec508e41e674b700ff3e
Karthees <kartheeswaran2201@gmail.com>
2026-03-26 16:21:52 +0530

    Create 152-MaximumProductSubarray

---
+++ 152-MaximumProductSubarray/152-MaximumProductSubarray.java
@@ -0,0 +1,23 @@
+// Last updated: 26/03/2026, 16:21:51
+class Solution {
+  public int maxProduct(int[] nums) {
+    int ans = nums[0];
+    int dpMin = nums[0];
+    int dpMax = nums[0];
+
+    for (int i = 1; i < nums.length; ++i) {
+      final int num = nums[i];
+      final int prevMin = dpMin;
+      final int prevMax = dpMax;
+      if (num < 0) {
+        dpMin = Math.min(prevMax * num, num);
+        dpMax = Math.max(prevMin * num, num);
+      } else {
+        dpMin = Math.min(prevMin * num, num);
+        dpMax = Math.max(prevMax * num, num);
+      }
+      ans = Math.max(ans, dpMax);
+    }
+    return ans;
+  }
+}
