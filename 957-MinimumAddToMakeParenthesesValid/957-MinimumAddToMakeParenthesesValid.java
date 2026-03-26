058c830bf1e1d93e032c7f51ac8866be71687ac5
Karthees <kartheeswaran2201@gmail.com>
2026-03-26 16:18:21 +0530

    Create 957-MinimumAddToMakeParenthesesValid

---
+++ 957-MinimumAddToMakeParenthesesValid/957-MinimumAddToMakeParenthesesValid.java
@@ -0,0 +1,16 @@
+// Last updated: 26/03/2026, 16:18:19
+class Solution {
+    public int minAddToMakeValid(String s) {
+     Stack <Character> st=new Stack<>();
+     
+     for(int i=0;i<s.length();i++)
+     {
+        char c=s.charAt(i);
+        if(!st.isEmpty() && (st.peek()=='(' && c==')'))
+        st.pop();
+        else
+        st.push(c);
+     }
+     return st.size();
+    }
+}
