feda989fec6d83db644a0772d309f055e16359bf
Karthees <kartheeswaran2201@gmail.com>
2026-05-26 23:27:38 +0530

    File updated at 26/05/2026, 23:27:37

--- 224-BasicCalculator/224-BasicCalculator.java
+++ 224-BasicCalculator/224-BasicCalculator.java
@@ -1,44 +1,16 @@
-// Last updated: 26/05/2026, 23:21:48
+// Last updated: 26/05/2026, 23:27:37
 1class Solution {
-2    public int calculate(String s) {
-3
-4        Stack<Integer> stack = new Stack<>();
-5
-6        int res = 0;
-7        int curr = 0;
-8        int sign = 1;
-9
-10        for (char c : s.toCharArray()) {
-11
-12            if (Character.isDigit(c)) {
-13                curr = curr * 10 + (c - '0');
-14            } 
-15            else if (c == '+') {
-16                res += curr * sign;
-17                sign = 1;
-18                curr = 0;
-19            } 
-20            else if (c == '-') {
-21                res += curr * sign;
-22                sign = -1;
-23                curr = 0;
-24            } 
-25            else if (c == '(') {
-26                stack.push(res);
-27                stack.push(sign);
-28                res = 0;
-29                sign = 1;
-30                curr = 0;
-31            } 
-32            else if (c == ')') {
-33                res += curr * sign;
-34                curr = 0;
-35                res *= stack.pop();
-36                res += stack.pop();
-37            }
-38        }
-39
-40        res += sign * curr;
-41        return res;
-42    }
-43}
+2  public int countDigitOne(int n) {
+3    int ans = 0;
+4    for (long pow10 = 1; pow10 <= n; pow10 *= 10) {
+5      final long divisor = pow10 * 10;
+6      final int quotient = (int) (n / divisor);
+7      final int remainder = (int) (n % divisor);
+8      if (quotient > 0)
+9        ans += quotient * pow10;
+10      if (remainder >= pow10)
+11        ans += Math.min(remainder - pow10 + 1, pow10);
+12    }
+13    return ans;
+14  }
+15}
