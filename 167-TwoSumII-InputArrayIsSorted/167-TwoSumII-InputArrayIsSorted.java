3034ec3ff63fa69b48dd95c59a9410d5b948216d
Karthees <kartheeswaran2201@gmail.com>
2026-06-01 09:42:34 +0530

    File updated at 01/06/2026, 09:42:34

--- 167-TwoSumII-InputArrayIsSorted/167-TwoSumII-InputArrayIsSorted.java
+++ 167-TwoSumII-InputArrayIsSorted/167-TwoSumII-InputArrayIsSorted.java
@@ -1,17 +1,14 @@
-// Last updated: 01/06/2026, 09:42:21
+// Last updated: 01/06/2026, 09:42:34
 1class Solution {
 2    public int[] twoSum(int[] nums, int t) {
-3        int n=nums.length;
-4        for(int i=0;i<n;i++)
-5        {
-6            for(int j=i+1;j<n;j++)
-7            {
-8                if(nums[i]+nums[j]==t)
-9                {
-10                    return new int[] {i+1,j+1};
-11                }
-12            }
-13        }
-14        return new int[] {0,0};
-15    }
-16}
+3        int n = nums.length;
+4        for (int i = 0; i < n; i++) {
+5            for (int j = i + 1; j < n; j++) {
+6                if (nums[i] + nums[j] == t) {
+7                    return new int[] { i + 1, j + 1 };
+8                }
+9            }
+10        }
+11        return new int[] { 0, 0 };
+12    }
+13}
