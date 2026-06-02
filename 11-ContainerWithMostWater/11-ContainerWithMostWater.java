584777564512f8c60983e183842654aec2f607c0
Karthees...! <kartheeswaran2201@gmail.com>
2026-06-02 07:33:21 +0530

    File updated at 02/06/2026, 07:33:20

--- 11-ContainerWithMostWater/11-ContainerWithMostWater.java
+++ 11-ContainerWithMostWater/11-ContainerWithMostWater.java
@@ -1,20 +1,24 @@
-// Last updated: 26/03/2026, 16:26:23
-class Solution {
-    public int maxArea(int[] arr) {
-        int n = arr.length - 1;
-        int l = 0;
-        int r = n;
-        int max = 0;
-        while (l < r) {
-            int w = Math.min(arr[l], arr[r]);
-            max = Math.max(max, w * n);
-            n--;
-            if (arr[l] < arr[r]) {
-                l++;
-            } else {
-                r--;
-            }
-        }
-        return max;
-    }
-}
+// Last updated: 02/06/2026, 07:33:20
+1class Solution {
+2    public int maxArea(int[] arr) {
+3        int n=arr.length-1;
+4        int l=0;
+5        int r=n;
+6        int max=0;
+7        while(l<r)
+8        {
+9            int w=Math.min(arr[l],arr[r]);
+10            max=Math.max(max,w*n);
+11            n--;
+12            if(arr[l]<arr[r])
+13            {
+14                l++;
+15            }
+16            else
+17            {
+18                r--;
+19            }
+20        }
+21        return max;
+22    }
+23}
