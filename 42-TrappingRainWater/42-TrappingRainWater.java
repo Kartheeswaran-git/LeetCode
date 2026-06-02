e7f5e983af26427013607958c8a3ed5b64c197d3
Karthees...! <kartheeswaran2201@gmail.com>
2026-06-02 07:45:14 +0530

    File updated at 02/06/2026, 07:45:13

--- 42-TrappingRainWater/42-TrappingRainWater.java
+++ 42-TrappingRainWater/42-TrappingRainWater.java
@@ -1,24 +1,24 @@
-// Last updated: 26/03/2026, 16:24:12
-class Solution {
-    public int trap(int[] height) {
-        int left = 0;
-        int right = height.length - 1;
-        int leftMax = height[left];
-        int rightMax = height[right];
-        int water = 0;
-
-        while (left < right) {
-            if (leftMax < rightMax) {
-                left++;
-                leftMax = Math.max(leftMax, height[left]);
-                water += leftMax - height[left];
-            } else {
-                right--;
-                rightMax = Math.max(rightMax, height[right]);
-                water += rightMax - height[right];
-            }
-        }
-
-        return water;        
-    }
-}
+// Last updated: 02/06/2026, 07:45:13
+1class Solution {
+2    public int trap(int[] height) {
+3        int left = 0;
+4        int right = height.length - 1;
+5        int leftMax = height[left];
+6        int rightMax = height[right];
+7        int water = 0;
+8
+9        while (left < right) {
+10            if (leftMax < rightMax) {
+11                left++;
+12                leftMax = Math.max(leftMax, height[left]);
+13                water += leftMax - height[left];
+14            } else {
+15                right--;
+16                rightMax = Math.max(rightMax, height[right]);
+17                water += rightMax - height[right];
+18            }
+19        }
+20
+21        return water;        
+22    }
+23}
