5e58e6b78ea8cfed9b47ee3e6aa32e61395e9f0a
Karthees...! <kartheeswaran2201@gmail.com>
2026-06-03 10:12:40 +0530

    File updated at 03/06/2026, 10:12:39

--- 80-RemoveDuplicatesfromSortedArrayII/80-RemoveDuplicatesfromSortedArrayII.java
+++ 80-RemoveDuplicatesfromSortedArrayII/80-RemoveDuplicatesfromSortedArrayII.java
@@ -1,16 +1,35 @@
-// Last updated: 03/06/2026, 09:16:09
-1class Solution {
-2    public int removeDuplicates(int[] nums) {
-3        int n = nums.length;
-4        if (n <= 2)
-5            return n;
-6        int j = 2;
-7        for (int i = 2; i < n; i++) {
-8            if (nums[i] != nums[j - 2]) {
-9                nums[j] = nums[i];
-10                j++;
-11            }
-12        }
-13        return j;
-14    }
-15}
+// Last updated: 03/06/2026, 10:12:39
+1// class Solution {
+2//     public void rotate(int[] nums, int k) {
+3//         int n = nums.length;
+4//         k = k % n;
+5//         for (int i = 0; i < k; i++) {
+6//             int t = nums[n - 1];
+7//             for (int j = n - 1; j > 0; j--) {
+8//                 nums[j] = nums[j - 1];
+9//             }
+10//             nums[0] = t;
+11//         }
+12//     }
+13// }
+14
+15class Solution {
+16    public void rotate(int[] nums, int k) {
+17        int n = nums.length;
+18        k = k % n;
+19
+20        int a = 0;
+21        int[] arr = new int[n];
+22
+23        for (int i = 0; i < n; i++) {
+24            if (n - k + a < n)
+25                arr[i] = nums[n - k + a++];
+26            else
+27                arr[i] = nums[(n - k + a++) % n];
+28        }
+29
+30        for (int i = 0; i < n; i++) {
+31            nums[i] = arr[i];
+32        }
+33    }
+34}
