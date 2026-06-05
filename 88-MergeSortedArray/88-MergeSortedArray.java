8828f5e912c472fe767f2277ca350bbd17522afe
Karthees...! <kartheeswaran2201@gmail.com>
2026-06-06 00:39:04 +0530

    File updated at 06/06/2026, 00:39:03

--- 88-MergeSortedArray/88-MergeSortedArray.java
+++ 88-MergeSortedArray/88-MergeSortedArray.java
@@ -1,13 +1,11 @@
-// Last updated: 06/06/2026, 00:06:37
+// Last updated: 06/06/2026, 00:39:03
 1class Solution {
-2    public int removeDuplicates(int[] n) {
-3        int j = 0;
-4        for (int i = 1; i < n.length; i++) {
-5            if (n[j] != n[i]) {
-6                n[++j] = n[i];
-7                
-8            }
-9        }
-10        return j+1;
-11    }
-12}
+2    public int strStr(String a, String b) {
+3        for(int i = 0, j = b.length(); j<=a.length(); i++,j++){
+4            if(a.substring(i,j).equals(b)){
+5                return i;
+6            }
+7        }
+8        return -1;
+9    }
+10}
