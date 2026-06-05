f1a7d8443eb443c49d1f2e593f3dff48df6835e1
Karthees...! <kartheeswaran2201@gmail.com>
2026-06-05 23:41:15 +0530

    File updated at 05/06/2026, 23:41:14

--- 88-MergeSortedArray/88-MergeSortedArray.java
+++ 88-MergeSortedArray/88-MergeSortedArray.java
@@ -1,11 +1,11 @@
-// Last updated: 26/03/2026, 16:23:21
-class Solution {
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
-        for(int i=m,j=0;i<nums1.length;i++)
-        {
-            nums1[i]=nums2[j];
-            j++;
-        }
-        Arrays.sort(nums1);
-    }
-}
+// Last updated: 05/06/2026, 23:41:14
+1class Solution {
+2    public void merge(int[] n1, int m, int[] n2, int n) {
+3        for(int i=m,j=0;i<n1.length;i++)
+4        {
+5            n1[i]=n2[j];
+6            j++;
+7        }
+8        Arrays.sort(n1);
+9    }
+10}
