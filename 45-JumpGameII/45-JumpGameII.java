603cd2f5ce0f149792519e60f8894c681aeeeaf4
Karthees...! <kartheeswaran2201@gmail.com>
2026-06-10 21:39:22 +0530

    File updated at 10/06/2026, 21:39:21

--- 45-JumpGameII/45-JumpGameII.java
+++ 45-JumpGameII/45-JumpGameII.java
@@ -1,18 +1,20 @@
-// Last updated: 05/06/2026, 01:20:51
+// Last updated: 10/06/2026, 21:39:21
 1class Solution {
-2    public int climbStairs(int n) {
-3        if (n <= 3) return n;
-4
-5        int prev1 = 3;
-6        int prev2 = 2;
-7        int cur = 0;
-8
-9        for (int i = 3; i < n; i++) {
-10            cur = prev1 + prev2;
-11            prev2 = prev1;
-12            prev1 = cur;
-13        }
-14
-15        return cur;        
-16    }
-17}
+2    public int jump(int[] nums) {
+3        int n = nums.length;
+4        if (n <= 1) return 0;
+5
+6        int j = 0;
+7        int c = 0;
+8        int max = 0;
+9        for (int i = 0; i < n - 1; i++) {
+10            max = Math.max(max, i + nums[i]);
+11            if (i == c) {
+12                j++;
+13                c = max;
+14                if (c >= n - 1) break;
+15            }
+16        }
+17        return j;
+18    }
+19}
