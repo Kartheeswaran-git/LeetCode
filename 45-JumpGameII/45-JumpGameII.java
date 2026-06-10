32b25a050119277513d4d2b31cde7bfd62bea89a
Karthees...! <kartheeswaran2201@gmail.com>
2026-06-10 23:42:55 +0530

    File updated at 10/06/2026, 23:42:55

--- 45-JumpGameII/45-JumpGameII.java
+++ 45-JumpGameII/45-JumpGameII.java
@@ -1,20 +1,13 @@
-// Last updated: 10/06/2026, 21:39:21
+// Last updated: 10/06/2026, 23:42:55
 1class Solution {
-2    public int jump(int[] nums) {
-3        int n = nums.length;
-4        if (n <= 1) return 0;
-5
-6        int j = 0;
-7        int c = 0;
-8        int max = 0;
-9        for (int i = 0; i < n - 1; i++) {
-10            max = Math.max(max, i + nums[i]);
-11            if (i == c) {
-12                j++;
-13                c = max;
-14                if (c >= n - 1) break;
-15            }
-16        }
-17        return j;
-18    }
-19}
+2    public int singleNumber(int[] nums) {
+3        Arrays.sort(nums);
+4        int n = nums.length;
+5        for (int i = 0; i < n - 1; i += 3) {
+6            if (i + 1 >= n || nums[i] != nums[i + 1]) {
+7                return nums[i];
+8            }
+9        }
+10        return nums[n - 1];
+11    }
+12}
