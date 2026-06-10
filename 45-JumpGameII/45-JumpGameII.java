95adae710c74d5ebb8340ee3450e3eee00723515
Karthees...! <kartheeswaran2201@gmail.com>
2026-06-11 00:41:59 +0530

    File updated at 11/06/2026, 00:41:59

--- 45-JumpGameII/45-JumpGameII.java
+++ 45-JumpGameII/45-JumpGameII.java
@@ -1,13 +1,24 @@
-// Last updated: 10/06/2026, 23:42:55
+// Last updated: 11/06/2026, 00:41:59
 1class Solution {
-2    public int singleNumber(int[] nums) {
-3        Arrays.sort(nums);
-4        int n = nums.length;
-5        for (int i = 0; i < n - 1; i += 3) {
-6            if (i + 1 >= n || nums[i] != nums[i + 1]) {
-7                return nums[i];
-8            }
-9        }
-10        return nums[n - 1];
-11    }
-12}
+2    public ListNode deleteDuplicates(ListNode head) {
+3
+4        if (head == null) return null;
+5        ListNode dummy = new ListNode(0);
+6        dummy.next = head;
+7        ListNode temp = dummy;
+8        ListNode prev = head;
+9        while (prev != null) {
+10            if (prev.next != null && prev.val == prev.next.val) {
+11                int val = prev.val;
+12                while (prev != null && prev.val == val) {
+13                    prev = prev.next;
+14                }
+15                temp.next = prev;
+16            } else {
+17                temp = prev;
+18                prev = prev.next;
+19            }
+20        }
+21        return dummy.next;
+22    }
+23}
