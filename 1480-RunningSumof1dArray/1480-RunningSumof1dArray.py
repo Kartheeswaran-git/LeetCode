e11cf8ba4063e5a22ef13ba1d0a927f93122f890
Karthees...! <kartheeswaran2201@gmail.com>
2026-06-13 12:26:27 +0530

    File updated at 13/06/2026, 12:26:26

--- 1480-RunningSumof1dArray/1480-RunningSumof1dArray.py
+++ 1480-RunningSumof1dArray/1480-RunningSumof1dArray.py
@@ -1,7 +1,13 @@
-# Last updated: 13/06/2026, 12:22:22
+# Last updated: 13/06/2026, 12:26:26
 1class Solution(object):
-2    def runningSum(self, nums):
-3        for i in range(0,len(nums)-1):
-4            nums[i+1]+=nums[i]
-5        return nums
-6        
+2    def maximumWealth(self, a):
+3        max=0
+4        for i in range(0,len(a)):
+5            sum=0
+6            for j in range(0,len(a[i])):
+7                sum+=a[i][j]
+8            if max<sum:
+9                max=sum
+10        return max
+11
+12        
