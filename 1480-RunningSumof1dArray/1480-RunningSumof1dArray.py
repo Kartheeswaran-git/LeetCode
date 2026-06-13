9fdd8afc9b4c9e762a249bce6db39187b15d00ea
Karthees...! <kartheeswaran2201@gmail.com>
2026-06-13 14:04:05 +0530

    File updated at 13/06/2026, 14:04:05

--- 1480-RunningSumof1dArray/1480-RunningSumof1dArray.py
+++ 1480-RunningSumof1dArray/1480-RunningSumof1dArray.py
@@ -1,14 +1,4 @@
-# Last updated: 13/06/2026, 12:36:38
+# Last updated: 13/06/2026, 14:04:05
 1class Solution(object):
-2    def fizzBuzz(self, n):
-3        a=[]
-4        for i in range(1,n+1):
-5            if(i%3==0 and i%5==0):
-6                a.append("FizzBuzz")
-7            elif(i%3==0):
-8                a.append("Fizz")
-9            elif(i%5==0):
-10                a.append("Buzz")
-11            else:
-12                a.append(str(i))
-13        return a
+2    def isAnagram(self, s, t):
+3        return(sorted(s)==sorted(t))
