1dfc0828350f1ba25be57c09c0dc23d9db019f83
Karthees...! <kartheeswaran2201@gmail.com>
2026-06-14 22:54:56 +0530

    File updated at 14/06/2026, 22:54:56

--- 135-Candy/135-Candy.java
+++ 135-Candy/135-Candy.java
@@ -1,17 +1,13 @@
-// Last updated: 14/06/2026, 22:40:29
+// Last updated: 14/06/2026, 22:54:56
 1class Solution {
-2    public boolean isIsomorphic(String s, String t) {
-3
-4        int map1[] = new int[200];
-5        int map2[] = new int[200];
-6        if (s.length() != t.length())
-7            return false;
-8        for (int i = 0; i < s.length(); i++) {
-9            if (map1[s.charAt(i)] != map2[t.charAt(i)])
-10                return false;
-11            map1[s.charAt(i)] = i + 1;
-12            map2[t.charAt(i)] = i + 1;
-13        }
-14        return true;
-15    }
-16}
+2    public boolean containsNearbyDuplicate(int[] nums, int k) {
+3        for (int i = 0; i < nums.length; i++) {
+4            for (int j = i + 1; j < nums.length && j <= i + k; j++) {
+5                if (nums[i] == nums[j]) {
+6                    return true;
+7                }
+8            }
+9        }
+10        return false;
+11    }
+12}
