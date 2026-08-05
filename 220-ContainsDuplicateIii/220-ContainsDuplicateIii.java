c2ed0544a9a369bbc8fb5d5f9d6a365354450aec
Karthees...! <kartheeswaran2201@gmail.com>
2026-08-05 10:29:50 +0530

    Create 220-ContainsDuplicateIii

---
+++ 220-ContainsDuplicateIii/220-ContainsDuplicateIii.java
@@ -0,0 +1,23 @@
+// Last updated: 05/08/2026, 10:29:49
+class Solution {
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int indexDiff, int valueDiff) {
+        if (indexDiff <= 0 || valueDiff < 0) return false;
+
+        TreeSet<Long> window = new TreeSet<>(); 
+        for (int i = 0; i < nums.length; i++) {
+            if (i > indexDiff) {
+                window.remove((long) nums[i - indexDiff - 1]);
+            }
+            long x = nums[i];
+            long low = x - (long) valueDiff;
+            long high = x + (long) valueDiff;
+            Long candidate = window.ceiling(low);
+            if (candidate != null && candidate <= high) {
+                return true;
+            }
+            window.add(x);
+        }
+
+        return false;
+    }
+}
