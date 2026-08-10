fa0bea5965fefae1dc1712737a6094f894ce0edb
Karthees...! <kartheeswaran2201@gmail.com>
2026-08-10 14:50:38 +0530

    File updated at 10/08/2026, 14:50:38

--- 551-StudentAttendanceRecordI/551-StudentAttendanceRecordI.py
+++ 551-StudentAttendanceRecordI/551-StudentAttendanceRecordI.py
@@ -1,11 +1,13 @@
-# Last updated: 10/08/2026, 14:31:18
+# Last updated: 10/08/2026, 14:50:38
 1class Solution(object):
-2    def restoreString(self, s, arr):
-3        ans=[None]*len(s)
-4        for i in range(len(s)):
-5            ans[arr[i]]=s[i]
-6        a=""
-7        for i in ans:
-8            a+=i
-9        return a
-10        
+2    def heightChecker(self, h):
+3        arr=[]
+4        for i in h:
+5            arr.append(i)
+6        arr.sort()
+7        c=0
+8        for i in range(len(h)):
+9            if h[i] != arr[i]:
+10                c+=1 
+11        return c
+12        
