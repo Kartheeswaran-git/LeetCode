36355eef137159c471b0cc14c8d2d4105af4102a
Karthees...! <kartheeswaran2201@gmail.com>
2026-08-10 14:31:18 +0530

    File updated at 10/08/2026, 14:31:18

--- 551-StudentAttendanceRecordI/551-StudentAttendanceRecordI.py
+++ 551-StudentAttendanceRecordI/551-StudentAttendanceRecordI.py
@@ -1,11 +1,11 @@
-# Last updated: 10/08/2026, 13:53:32
+# Last updated: 10/08/2026, 14:31:18
 1class Solution(object):
-2    def checkRecord(self, s):
-3        a=0
-4        for i in s:
-5            if i=='A':
-6                a+=1
-7        if a>=2 or "LLL" in s:
-8            return False
-9        return True
+2    def restoreString(self, s, arr):
+3        ans=[None]*len(s)
+4        for i in range(len(s)):
+5            ans[arr[i]]=s[i]
+6        a=""
+7        for i in ans:
+8            a+=i
+9        return a
 10        
